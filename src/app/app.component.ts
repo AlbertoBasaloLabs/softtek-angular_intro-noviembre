3f50bb70e3ba57c2e9f8f7d1603fe42c65a8b895
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-03 17:38:57 +0100

    feat: agencies list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,29 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   template: `
-    <h1>Softtek Angular Intro</h1>
+    <h1>{{ title }}</h1>
+    <h3>We work with {{ activeAgenciesCounter }} agencies</h3>
+    <ul>
+      <ng-container *ngFor="let agency of agencies">
+        <li *ngIf="agency.status === 'Active'">
+          <span>{{ agency.name | uppercase }}</span>
+          <span *ngIf="agency.range === 'Interplanetary'">🪐</span>
+          <span *ngIf="agency.range === 'Orbital'">🌍</span>
+        </li>
+      </ng-container>
+    </ul>
     <router-outlet></router-outlet>
   `,
   styles: [],
 })
 export class AppComponent {
   title = "sofftek-angular_intro-noviembre";
+  agencies = [
+    { name: "Space X", range: "Interplanetary", status: "Active" },
+    { name: "Blue Origin", range: "Orbital", status: "Active" },
+    { name: "Virgin Galactic", range: "Orbital", status: "Pending" },
+  ];
+  activeAgenciesCounter = this.agencies.filter(
+    (agency) => agency.status === "Active"
+  ).length;
 }
